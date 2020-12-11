fcfb87acaf8adfc72946302c1108cad9d218b552
JamesHughes85 <74372172+JamesHughes85@users.noreply.github.com>
2020-12-11 12:50:41 +0000

    Task 1

--- lab-programArrayPtr/lab3/functions.h
+++ lab-programArrayPtr/lab3/functions.h
@@ -1,1 +1,6 @@
 
+void LabInfo() {
+    cout << "James Hughes\n";
+    cout << "Lab 3\n" << endl;
+}
+
--- lab-programArrayPtr/lab3/main.cpp
+++ lab-programArrayPtr/lab3/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
 using namespace std;
+#include "functions.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+LabInfo();
+
+return 0;
 }
