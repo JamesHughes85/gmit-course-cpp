5d08e2b1ebbf716540d6bb035f8109e3af7f8cd8
JamesHughes85 <74372172+JamesHughes85@users.noreply.github.com>
2020-12-15 16:19:18 +0000

    Lab 3

--- lab-programArrayPtr/lab3/functions.h
+++ lab-programArrayPtr/lab3/functions.h
@@ -4,3 +4,18 @@ void LabInfo() {
     cout << "Lab 3\n" << endl;
 }
 
+void Tree () {
+    char cTree;
+    int iSize;
+    int iSpace;
+    int iChars;
+    cout << "Enter the height of your Christmas tree: ";
+    cin >> iSize;
+    cout << "Enter the character you would like to use for your tree: ";
+    cin >> cTree;
+    for(int i = 0; i < iSize; i++){
+    iSpace = (iSize-i)-1;
+    iChars = (2*i)+1;
+    string sSpace(iSpace, '_');
+    string sTree(iChars, cTree);
+    cout << sSpace << sTree << sSpace << endl;}}
--- lab-programArrayPtr/lab3/main.cpp
+++ lab-programArrayPtr/lab3/main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 using namespace std;
+#include <string>
 #include "functions.h"
 
 int main()
 {
-LabInfo();
+LabInfo(); //Task 1
+
+system("pause");
+
+//Task 2
+cout<<"\nQuestion 2.\n";
+    int Array[6];
+    int x;
+    int sca;
+    int w;
+    cout<<"Please Enter in 6 Numbers:\n";
+    for (int n=0;n<6;n++){
+    cout<<"Please Enter number and hit Return: ";
+    cin>>Array[n];
+    if(Array[n]>100||Array[n]<0){
+    n=x;
+    cout<<"Number must be between 0 and 100!\n";}
+    }
+    cout<<"The Array you Entered is: ";
+    for (int n=0;n<6;n++){
+    cout<<Array[n]<<",";
+}
+system("pause\n");
+
+cout <<"Task 7\n"<<endl;
+
+Tree();
+
 
 return 0;
 }
