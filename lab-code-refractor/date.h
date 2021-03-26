2e6086d77f5dd799f77d8fcd811b24851f59b2aa
JamesHughes85 <74372172+JamesHughes85@users.noreply.github.com>
2021-03-26 13:43:42 +0000

    Lab 7

---
+++ lab-code-refractor/date.h
@@ -0,0 +1,14 @@
+//James Hughes - Lab 7 - 26/03/21
+
+class date{
+private:
+    int month;
+    string year;
+
+public:
+    void setDate(string year){
+        this->year = year;
+    }
+
+
+};
---
+++ lab-code-refractor/player.h
@@ -0,0 +1,38 @@
+//James Hughes - Lab 7 - 26/03/21
+#include <iostream>
+
+using namespace std;
+
+
+
+class Player{
+private:
+    string forename;
+    string surname;
+    string position;
+    string mobileNumber;
+    string Gender;
+    int dob;
+
+public:
+    Player();
+    Player(string forename, string surname);
+    Player(string forename, string surname, int mobileNumber, string position);
+
+    void printPlayerInfo();
+    string getForename();
+    string getSurname();
+    void setName(string name);
+    void setName(string forename,string surname);
+
+    void setPosition(string position);
+    string getPosition();
+    void setMobileNumber(string mobileNumber);
+    string getMobileNumber();
+
+
+    void setDOB(int dob);
+    int getDOB();
+
+};
+
